3c875165f5b626cc4e1c92027dd5c5393213b4e4
Your Name <you@example.com>
2022-12-14 12:41:13 +0300

    done

--- index.js
+++ index.js
@@ -4,25 +4,26 @@ function getMovies(){
  .then(res => res.json())
  .then(data  =>{
     console.log(data)
-    data.forEach(data =>(
+    data.forEach(data =>()}
     let ul = getElementById(list)   
-    ))
-    });
+    
 }
+
+
 getMovies()
 
-// function diti(movi)
+function diti(movi)
 
-//     console.log(movi.title);
-//     inf(movi)
+ console.log(movi.title);
+  inf(movi)
     
-// console.log("helllo");
+console.log("helllo");
 
-// function inf(movi){
-//     fetch("http://localhost:3000/films")
-//     console.log(movi.description);
-//     console.log(movi.capacity);
-// }
+function inf(movi){
+ fetch("http://localhost:3000/films")
+console.log(movi.description);
+console.log(movi.capacity);
+}
 
-// getElementById(data)
+getElementById(data)
 
